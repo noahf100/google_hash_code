77b41c37f66388a6b2457f1abb06849828a648e6
Noah  Fang <NoahFang@c054217.wlan.net.ed.ac.uk>
2019-02-28 18:38:46 +0000

    asdf

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import heapq
+
 # input() reads a string with a line of input, stripping the '\n' (newline) at the end.
 # This is all you need for most Kickstart problems.
 t = int(input())  # read a line with a single integer
@@ -9,3 +11,50 @@ for i in range(1, t + 1):
 
   print("Case #{}: {} {}".format(i, n + m, n * m))
   # check out .format's specification for more formatting options
+
+def isH(index):
+	return _[index][0] == "H"
+
+def do(visited = {}):
+	res = []
+    heap = []
+
+	for i in _.keys():
+		runningMax = -1
+		maxNode = None
+		prevNode = i
+
+		localVisited = visited
+		localPath = [i]
+		localSum = 0
+
+		while(True):
+			for j in _.keys():
+				if j in visited:
+					continue
+				tmp = score(i, j)
+				if tmp > runningMax:
+					runningMax = tmp
+					maxNode = j
+
+			if runningMax <= -1:
+				break
+			#elif runningMax == 0:
+			#	break
+			else:
+				localPath.append(j)
+				visited.add(j)
+				prevNode = j
+				localSum += runningMax
+				if not isH(j):
+					break
+
+		heapq.push(heap, (-localSum, localPath))
+
+
+	path = heappop(heap)
+	path = path[1]
+	while(not isH(path[-1])):
+		
+
+
